743d49d3a2954576dac8f64161462747952c0016
Masha Vainblat <mashavainblat@gmail.com>
2017-07-19 22:01:36 -0400

    adding page-introductory.php

---
+++ wp-content/themes/delcanCo2017/page-introductory.php
@@ -0,0 +1,42 @@
+<?php get_header();?>
+
+	<div class="site-content clearfix" id="introductoryPage">
+		
+		<div class="main-column">
+			
+			<?php
+			### Loop code
+			# if we have posts, do something:
+			if(have_posts()) :
+				#while we have posts, do something with each post
+				while(have_posts()) : the_post(); ?>
+				
+				<article class="post page">
+					<!-- <h1><?php #the_title(); ?></h1> -->
+					<?php the_content(); ?>
+				</article>
+				<?php endwhile;
+
+				else :
+					echo '<p>No content found</p>';
+				endif;
+			?>
+
+		</div>	<!-- .main-column -->
+	</div> <!-- .site-content .clearfix -->
+
+	<footer class="site-footer">
+		<div class="row">
+			<div class="col-lg-6 noPadding" id="address">
+				<p>147 W 35th St. Suite 202</p>
+				<p>NY, New York, 10001</p>
+			</div>
+
+			<div class="col-lg-6 noPadding" id="copyright">
+				<p>&copy; <?php echo date('Y');?> <?php bloginfo('name'); ?></p>
+			</div>
+		</div> <!-- row -->
+	</footer>
+
+
+<?php get_footer();?>
