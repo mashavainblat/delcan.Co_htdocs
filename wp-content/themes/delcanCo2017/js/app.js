8f155e4edd2e1e7da18079e8b415def63973c462
Masha Vainblat <mashavainblat@gmail.com>
2017-07-19 21:46:23 -0400

    adding js folder

---
+++ wp-content/themes/delcanCo2017/js/app.js
@@ -0,0 +1,44 @@
+$( document ).ready(function() {
+    console.log( "Hello World!" );
+
+	// $("body").click(function(){
+	// 	$("body").toggleClass("inverse");
+	// });
+	// $("body").click(function(){
+	// 	$("a").toggleClass("invertNav");
+	// });
+	// $("body").click(function(){
+	// 	console.log($(".arrows"));
+	// 	$(".arrows").toggleClass("invertArrows")
+
+	// })
+
+	// $('.grid').masonry({
+	//   itemSelector: '.grid-item',
+	//   columnWidth: '.grid-sizer'
+	//   // columnWidth: 240
+	// });
+
+// $('.grid').isotope({
+//   itemSelector: '.grid-item',
+//   percentPosition: true,
+//   masonry: {
+//     columnWidth: '.grid-sizer'
+//   }
+// });
+
+	// $('.grid').isotope({
+	//   // options
+	//   itemSelector: '.grid-item',
+	//   layoutMode: 'fitRows'
+	// });
+
+
+});
+
+
+// $("button").mouseenter(function (){
+//   var srcI = $(".show").attr("src");
+//   srcI = srcI.replace("thumb","large");
+//   $(".show").attr("src",srcI);
+// });
